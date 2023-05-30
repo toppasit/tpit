39ff656df6204226debd7a33ed3e76674f5c6d3a
pasit <>
2023-05-30 16:48:52 +0700

    show football page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Mainpage from './page/mainpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App />
+    {/* <App /> */}
+    <Mainpage/>
   </div>
 );
 
